bb823a3ee308ac6f4fb49178597f9e8d5728e28b
Habakuk Conrado <habakuk.conrado@gmail.com>
2025-05-08 14:15:06 -0300

    json altered

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/jamericomiranda.github.io/',  // 👈 Replace with your repo name
+  base: '/',  // 👈 Replace with your repo name
   plugins: [react()],
 })
